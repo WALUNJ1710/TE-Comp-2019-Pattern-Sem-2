2ab4855c34141ec1b146c5269c46e3e6b18c6d3e
PRATIK WALUNJ <92929453+WALUNJ1710@users.noreply.github.com>
2023-05-29 18:20:36 +0530

    Update DFS.py

--- Laboratory_Practice2(Artificial Intelligence)/BFS-DFS/DFS.py
+++ Laboratory_Practice2(Artificial Intelligence)/BFS-DFS/DFS.py
@@ -2,22 +2,22 @@ visited = set()
 
 def dfs(visited, graph, node):
     if node not in visited:
-        print(node,end = " ")
+        print(node, end=" ")
         visited.add(node)
         for neighbour in graph[node]:
             dfs(visited, graph, neighbour)
 
-#Driver Code
-print("Enter the number of nodes in the graph :")
+# Driver Code
+print("Enter the number of nodes in the graph:")
 num_nodes = int(input())
 
 graph = {}
-
 for i in range(num_nodes):
-    print("Enter the numbers for node {i}: ")
-    neighbours = input().split()
-    graph[str(i)] = neighbours
+    print(f"Enter the neighbors for node {i}: (separated by space, or press enter for no neighbors)")
+    neighbors = input().split()
+    graph[str(i)] = neighbors
 
-start_node = input("Enter the start node for Depth First Search")
-print("Following is a depth first search")
+start_node = input("Enter the start node for Depth-First Search:")
+
+print("Following is the Depth-First Search:")
 dfs(visited, graph, start_node)
