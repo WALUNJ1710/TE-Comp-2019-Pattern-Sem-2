9b8328e1f689f480c90383e532c160415ea53de5
PRATIK WALUNJ <92929453+WALUNJ1710@users.noreply.github.com>
2023-05-29 22:41:21 +0530

    Update N-queen.py

--- Laboratory_Practice2(Artificial Intelligence)/N-queen-Problem/N-queen.py
+++ Laboratory_Practice2(Artificial Intelligence)/N-queen-Problem/N-queen.py
@@ -1,69 +1,49 @@
 import numpy as np
 
-global n
-n = int(input("Enter the shape of the board:"))
-
-
 def printsolution(board):
     for i in range(n):
         for j in range(n):
             print(board[i][j], end=" ")
         print()
 
-
 def safety(board, row, col):
-    # check for the presense of any queen in a row
+    # Check for the presence of any queen in the same row
     for i in range(col):
         if board[row][i] == 1:
             return False
-        # for upper diagonal elements
+    # Check upper diagonal elements
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-        # for lower diagonal elements
+    # Check lower diagonal elements
     for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
     return True
 
-
 def solve(board, col):
     if col >= n:
         return True
-    # Exploring board to put the queen at correct position
-    for i in range(n):
-        # check if the position is safe or not and then put the queen
-        if safety(board, i, col):
-            board[i][col] = 1
-            # recurse for the next possible position for new QUEEN
-            if solve(board, col + 1) == True:
-                return True
-            # If there is no solution existing for queen make it zero
-            board[i][col] = 0
-    return False
-    # driver code
 
+    # Sort rows based on the number of conflicts with queens in previous columns
+    sorted_rows = sorted(range(n), key=lambda row: sum(board[row][:col]))
+
+    for row in sorted_rows:
+        if safety(board, row, col):
+            board[row][col] = 1
+            if solve(board, col + 1):
+                return True
+            board[row][col] = 0
+    return False
 
 def solution():
-    '''board =[[0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-          ]'''
     board = np.zeros(shape=(n, n), dtype=np.int64)
     if solve(board, 0) == False:
-        print("no solution exist")
+        print("No solution exists.")
         return False
     printsolution(board)
     return True
 
-
+# Driver code
+n = int(input("Enter the shape of the board:"))
 solution()
-
-
-
-
