ff97f52bcadd0bae409332e56ba6fc8588154c7e
PRATIK WALUNJ <92929453+WALUNJ1710@users.noreply.github.com>
2023-05-29 18:31:42 +0530

    Update Prims_Algorithm.py

--- Laboratory_Practice2(Artificial Intelligence)/Greedy_Search/Prims_Algorithm.py
+++ Laboratory_Practice2(Artificial Intelligence)/Greedy_Search/Prims_Algorithm.py
@@ -5,75 +5,85 @@ class Graph():
         self.V = vertices
         self.graph = [[0 for column in range(vertices)]
                       for row in range(vertices)]
- 
+
     # A utility function to print
     # the constructed MST stored in parent[]
     def printMST(self, parent):
         print("Edge \tWeight")
         for i in range(1, self.V):
             print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
- 
+
     # A utility function to find the vertex with
     # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
+    # not yet included in the shortest path tree
     def minKey(self, key, mstSet):
- 
         # Initialize min value
         min = sys.maxsize
- 
         for v in range(self.V):
             if key[v] < min and mstSet[v] == False:
                 min = key[v]
                 min_index = v
- 
         return min_index
- 
+
     # Function to construct and print MST for a graph
     # represented using adjacency matrix representation
     def primMST(self):
- 
         # Key values used to pick minimum weight edge in cut
         key = [sys.maxsize] * self.V
         parent = [None] * self.V  # Array to store constructed MST
-        # Make key 0 so that this vertex is picked as first vertex
+        # Make key 0 so that this vertex is picked as the first vertex
         key[0] = 0
         mstSet = [False] * self.V
- 
-        parent[0] = -1  # First node is always the root of
- 
+
+        parent[0] = -1  # First node is always the root of the MST
+
         for cout in range(self.V):
- 
             # Pick the minimum distance vertex from
             # the set of vertices not yet processed.
-            # u is always equal to src in first iteration
+            # u is always equal to src in the first iteration
             u = self.minKey(key, mstSet)
- 
+
             # Put the minimum distance vertex in
             # the shortest path tree
             mstSet[u] = True
- 
-            # Update dist value of the adjacent vertices
+
+            # Update the distance value of the adjacent vertices
             # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
+            # distance is greater than the new distance and
+            # the vertex is not in the shortest path tree
             for v in range(self.V):
- 
-                # graph[u][v] is non zero only for adjacent vertices of m
-                # mstSet[v] is false for vertices not yet included in MST
+                # graph[u][v] is non-zero only for adjacent vertices of u
+                # mstSet[v] is False for vertices not yet included in MST
                 # Update the key only if graph[u][v] is smaller than key[v]
-                if self.graph[u][v] > 0 and mstSet[v] == False \
-                and key[v] > self.graph[u][v]:
+                if (
+                    self.graph[u][v] > 0
+                    and mstSet[v] == False
+                    and key[v] > self.graph[u][v]
+                ):
                     key[v] = self.graph[u][v]
                     parent[v] = u
- 
+
         self.printMST(parent)
 
-if __name__ == '__main__':
-    g = Graph(5)
-    g.graph = [[0, 2, 0, 6, 0],
-               [2, 0, 3, 8, 5],
-               [0, 3, 0, 0, 7],
-               [6, 8, 0, 0, 9],
-               [0, 5, 7, 9, 0]]
- 
+if __name__ == "__main__":
+    num_vertices = int(input("Enter the number of vertices: "))
+    
+    g = Graph(num_vertices)
+    
+    print("Enter the adjacency matrix (space-separated values).")
+    print("Enter '0' for no edge between vertices.")
+    
+    for i in range(num_vertices):
+        row = list(map(int, input(f"Enter row {i + 1}: ").split()))
+        g.graph[i] = row
+    
     g.primMST()
+    
+#Enter the number of vertices: 5
+#Enter the adjacency matrix (space-separated values).
+#Enter '0' for no edge between vertices.
+#Enter row 1: 0 2 0 6 0
+#Enter row 2: 2 0 3 8 5
+#Enter row 3: 0 3 0 0 7
+#Enter row 4: 6 8 0 0 9
+#Enter row 5: 0 5 7 9 0
